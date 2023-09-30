34bf680e0dbf0b45ab599be942cfdaba51f1ed0e
yujiuqie <yujiuqie@163.com>
2023-09-30 11:03:33 +0800

    update config

--- Pods/Target Support Files/Pods-AFJ-Visual-Swift-AFJ-Visual-SwiftUITests/Pods-AFJ-Visual-Swift-AFJ-Visual-SwiftUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AFJ_Visual_Swift_AFJ_Visual_SwiftUITests : NSObject
-@end
-@implementation PodsDummy_Pods_AFJ_Visual_Swift_AFJ_Visual_SwiftUITests
-@end
--- Pods/Target Support Files/Pods-AFJ-Visual-SwiftTests/Pods-AFJ-Visual-SwiftTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AFJ_Visual_SwiftTests : NSObject
-@end
-@implementation PodsDummy_Pods_AFJ_Visual_SwiftTests
-@end
